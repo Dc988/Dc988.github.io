c24ee985305eebcf6a2b2a9985667bc0de633e3b
Dicmar Castro Dominguez <90455988+Dc988@users.noreply.github.com>
2025-08-18 15:28:06 -0500

    MODIFICACION DE PRESENTACION DE PROYECTOS
    SE MODIFICA EL FORMATO DE PRESENTACION DE PROYECTOS

--- js/main.js
+++ js/main.js
@@ -1,22 +1,27 @@
 // ADD CONTCT INFO
 function addContactInfo(name,url, icon, otherClass){
+    // añade informacion en header
     header_contact.innerHTML += `<a aria-label="${name}" href="${url}" target="_blank" class="ml-3 btn ${otherClass}"><i class="${icon}"></i></a>`;
+    // añade informacion en footer
     footer_contact.innerHTML += `<a href="${url}" aria-label="${name}" target="_blank"><i class="${icon}" ></i></a>`
 }
+
 // ADD SKILL
 function addSkill(title, image,prom) {
     skill_content.innerHTML +=
         `<div class="item">
         <div class="icon">
-            <i class="fa-brands ${image} fa-xl"></i>
+            <i class=" ${image}"></i>
         </div>
         <div class="skill-info">
             <h6 class="skill-name">${title}</h6>
             <h6 class="skill-per">${prom}</h6>
         </div>
     </div>`;
-
 }
+
+// add img gallery
+
 
 // PROYECT
 class Portafolio {
@@ -67,8 +72,9 @@ class Portafolio {
         });
     }
 
-    addTec(tec) {
-        this.tec.push(tec);
+    addTec(tec, icon) {
+
+        this.tec.push([tec, icon]);
         return this;
     }
 
@@ -79,61 +85,45 @@ class Portafolio {
 
     push() {
 
-        var tec = this.tec.map((e) => `<h5><span class="m-1 px-3 badge rounded-pill text-bg-secondary">${e}</span></h5>`).join(" ");
-
-        var btn_indicators = this.carrusel.map((e, i) => `<button class="active" type="button" data-bs-target="#${this.id}" data-bs-slide-to="${i}" aria-label="Slide ${i + 1}"></button>`).join(" ");
         var carousel_img = this.carrusel.map((e, i) =>
-            `<div class="carousel-item ${i==0?"active":""}">
-                <!--<a href="${e}" data-lightbox="${this.id}" >-->
-                    <img src="${e}" class="d-block w-100"
-                        alt="item-${i}.png">
-            <!-- </a>-->
-            </div>`).join(" ");
+            `<a data-fancybox="gallery_${this.filter}" href="${e}">
+                    <img src="${e}" alt="${this.title}_${i}"/>
+                </a>`).join(" ");
 
-        portfolio_container.innerHTML +=
-            `<div class="row portfolio-item m-2">
-                
-                <h2 class="text-center font-weight-bold portafolio-title my-3">${this.title}</h2>
-                <div id="${this.id}" class="carousel slide col-md-7" data-bs-theme="dark">
-                    <div class="carousel-indicators">
-                        ${btn_indicators}
-                    </div>
-                    <div class="carousel-inner">
-                        ${carousel_img}
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#${this.id}"
-                        data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#${this.id}"
-                        data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
+        var tec = this.tec.map((e) => `<h5><span class='m-1 px-3 badge rounded-pill text-bg-secondary'>${e[0]}</span></h5>`).join(' ');
+
+        var data_caption = `
+        <div class='col-md-5 m-auto'>
+            <div class='description'>
+                <p class='text-desc'>${this.descripcion}</p>
+            </div>
+            <div class='tecno'>
+                <h3 class='modal-title fs-5'>Tecnologías Utilizadas:</h3>
+                <div class='d-flex justify-content-start'>
+                    ${tec}
                 </div>
-                <div class="col-md-5 ">
-                    <div class="description">
-                        <h3 class="modal-title fs-5">Descripción:</h3>
-                        <p class="text-desc">${this.descripcion}</p>
-                    </div>
-                    <div class="tecno">
-                        <h3 class="modal-title fs-5">Tecnologías Utilizadas:</h3>
-                        <div class="d-flex justify-content-start">
-                            ${tec}
-                        </div>
-                    </div>
+            </div>
+        </div>`;
 
-                    <!-- <div class="modulos">
-                        <h2 class="modal-title fs-5" >Módulos y Funcionalidades:</h2>
-                        <ul class="mb-4">
-                            <li class="m-1" ></li>
-                        </ul>
-                    </div>-->
+        var tec = this.tec.map((e) => ` <i class="${e[1]}"></i>`).join(" ");
+        const card = document.createElement("div");
+        card.classList.add('grid-item', 'wow', 'zoomIn','m-3',this.filter);
+        card.innerHTML =`
+        <div class="img-place" data-src="${this.carrusel[0]}" data-fancybox="gallery_${this.filter}" data-caption="${data_caption}" >
+            <img src="${this.carrusel[0]}" alt="">
+            <div class="img-caption">
+                <h5 class="fg-theme">${this.title}</h5>
+                <div class="img-tec">
+                ${tec}
                 </div>
-
-            
+            </div>
+            <div style="display:none">
+                ${carousel_img}
+            </div>
         </div>`
+
+        portfolio_container.appendChild(card);
+        this.clear();
 
     }
 }
@@ -148,25 +138,24 @@ addContactInfo("Linkedin","https://www.linkedin.com/in/dicmar-andres-castro-domi
 addContactInfo("Whatsapp","https://wa.me/573043821671","fa-brands fa-whatsapp","btn-success")
 
 //HABILIDADES
-addSkill("HTML", "fa-html5","intermedio");
-addSkill("CSS", "fa-css3-alt","intermedio");
-addSkill("JAVASCRIPT", "fa-js","intermedio");
-addSkill("BOOTSTRAP", "fa-bootstrap","intermedio");
-addSkill("PHP", "fa-php","intermedio");
-addSkill("PYTHON", "fa-python","intermedio");
-addSkill("EXCEL VBA", "fa fa-file-xlsx","intermedio");
+addSkill("HTML", "fa-brands fa-html5","intermedio");
+addSkill("CSS", "fa-brands fa-css3-alt","intermedio");
+addSkill("JAVASCRIPT", "fa-brands fa-js","intermedio");
+addSkill("BOOTSTRAP", "fa-brands fa-bootstrap","intermedio");
+addSkill("PHP", "fa-brands fa-php","intermedio");
+addSkill("PYTHON", "fa-brands fa-python","intermedio");
+addSkill("EXCEL VBA", "fa-solid fa-file-excel","intermedio");
 
 //PORTAFOLIO
 var portfolio = new Portafolio();
 
 //1. GESTOR PACIENTES EME COLOMBIA VBA
-portfolio.setTitle("GESTOR PACIENTES EME COLOMBIA VBA")
+portfolio.setTitle("GESTOR PACIENTES")
     .setId("carousel_proyect_eme_excel")
-    .setFilter("excel")
+    .setFilter("VBA_EXCEL")
     .setDescripcion("Es un aplicativo de Excel, formulario de captura de información para siniestros SOAT, en base a la información solicitada por siras, creación de una base de datos para control y seguimiento de auditoría, facturación, pagos y glosas")
 
-    .addTec("EXCEL")
-    .addTec("MACROS")
+    .addTec("EXCEL","fa-solid fa-file-excel color-7")
 
     .addCarrusel("img/carouserl_proyectos/eme_excel/item-2.jpg")
     .addCarrusel("img/carouserl_proyectos/eme_excel/item-1.jpg")
@@ -175,7 +164,6 @@ portfolio.setTitle("GESTOR PACIENTES EME COLOMBIA VBA")
     .addCarrusel("img/carouserl_proyectos/eme_excel/item-5.jpg")
     .addCarrusel("img/carouserl_proyectos/eme_excel/item-6.jpg")
     .push();
-
+    
 // agregar btns de filtrado
 portfolio.setBtnFilter();
-
